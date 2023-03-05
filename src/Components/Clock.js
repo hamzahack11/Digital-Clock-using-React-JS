8b38e916c61bad732e6b10e0a1df4d54129d773f
hamzahack11 <hamzahack11@gmail.com>
2023-03-05 08:48:03 -0500

    simpleclock

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,88 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./Clock.css";
-
-function DigitalClock() {
-  // to get complete date liek day, month & year.
-
-  var today = new Date();
-  const dayNumber = today.getDate();
-  const year = today.getFullYear();
-  const dayName = today.toLocaleString("default", { weekday: "long" });
-  const monthName = today.toLocaleString("default", { month: "long" });
-  var minutes = today.getMinutes();
-  var seconds = today.getSeconds();
-  var hours = today.getHours();
+function Clock() {
   const [time, setTime] = useState(new Date());
+  const [is24Hour, setIs24Hour] = useState(false);
 
   useEffect(() => {
-    const intervalID = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-    return () => {
-      clearInterval(intervalID);
-    };
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(interval);
   }, []);
 
   const formatTime = (time) => {
-    if (timeFormat === "12") {
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+
+    if (is24Hour) {
+      return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+    } else {
+      let amPm = hours >= 12 ? 'PM' : 'AM';
       hours = hours % 12 || 12;
-      return "0" + hours;
-    } else {
-      hours = hours < 10 ? "0" + hours : hours;
-      return `${hours}`;
+      return `${hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds} ${amPm}`;
     }
   };
 
-  const addZero = (e) => {
-    if (e < 10) {
-      return (e = "0" + e);
-    } else {
-      return e;
-    }
-  };
-
-  const [timeFormat, setTimeFormat] = useState("12");
-
-  const handleFormatToggle = () => {
-    setTimeFormat(timeFormat === "12" ? "24" : "12");
+  const toggleTimeFormat = () => {
+    setIs24Hour(!is24Hour);
   };
 
   return (
-    <div className="container">
-      <div className="digital-clock">
-        <ul>
-          <li className="menu-item">
-            <span className="clock-formate-text">Switch Time Format</span>
-            <div
-              onClick={handleFormatToggle}
-              className={
-                timeFormat === "12"
-                  ? "format-switch-btn "
-                  : "format-switch-btn active"
-              }
-            ></div>
-          </li>
-        </ul>
-        <div className="time">
-          <span className="hour">{formatTime(time)}</span>
-          <span className="dots">:</span>
-          <span className="minutes">{addZero(minutes)}</span>
-          <div className="right-side">
-            <span className="period">{hours >= 12 ? "" : "AM"}</span>
-            <span className="seconds">{addZero(seconds)}</span>
-          </div>
-        </div>
-
-        <div className="calendar">
-          <span className="day-name">{dayName} :</span>
-          <span className="month-name">{monthName} :</span>
-          <span className="day-number">{dayNumber} :</span>
-          <span className="Year">{year}</span>
-        </div>
-      </div>
+    <div>
+      <h1>{formatTime(time)}</h1>
+      <button onClick={toggleTimeFormat}>{is24Hour ? 'Switch to 12-hour clock' : 'Switch to 24-hour clock'}</button>
     </div>
   );
 }
 
-export default DigitalClock;
+export default Clock;
